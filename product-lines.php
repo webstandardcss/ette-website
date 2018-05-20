3b88cb5dd3912d86cbbdb503c355fdf2457702e5
webstandardcss <webstandardcss@gmail.com>
2018-05-20 01:17:01 -0500

    product-lines.php created.  TODO: need images downloaded from old site

---
+++ product-lines.php
@@ -0,0 +1,437 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "Homepage";
+$pageTitleTag = $pageTitle . " " . $siteName;
+$pageDescription = $siteName  . "  " . $pageTitle;
+$pageKeywords = $pageTitle . "  " . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+<div class="main-outer">
+  <aside class="main-content-before"></aside>
+  <main class="main-content">
+    <h4 class="main-description">Product Lines</h4>      
+    <article class="product-lines flex-center">
+      <figure class="product-lines-item">
+        <a href="https://www.airliftcompany.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/Airlift_Logo.jpg" alt="Air Lift" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="https://www.airliftcompany.com/">Airlift</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.airaid.com" target="_new">
+          <img src="/files/2011/11/airaid.jpg" alt="Airaid" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.airaid.com" target="_new">Airaid</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.amp-research.com/" target="_new">
+          <img src="/files/2011/12/amp-research.jpg" alt="Amp Research" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.amp-research.com/">Amp Research</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="https://blackjacktirerepair.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/Black-Jack-Logo.png" alt="Blackjack Tire Repair" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="https://blackjacktirerepair.com/" target="_new">Black Jack Tire Repair</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.bodyguardbumpers.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/Bodyguard-Bumpers-logo.png" alt="Bodyguard Bumpers" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.bodyguardbumpers.com/" target="_new">Bodyguard Bumpers</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.boltlock.com/" target="_new">
+          <img src="/files/2011/12/boltlock.jpg" alt="Boltlock" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.boltlock.com/" target="_new">Boltlock</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.turnoverball.com/" target="_new">
+          <img src="/files/2011/11/bw-hitches.jpg" alt="B&amp;W Hitches" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.turnoverball.com/" target="_new">B&amp;W Hitches</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.blueox.us" target="_new">
+          <img src="/files/2011/11/blue_ox.jpg" alt="Blue Ox Towing Products" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.blueox.us" target="_new">Blue Ox Towing Products</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.bulldogwinch.com/" target="_new">
+          <img src="/files/2011/11/bulldogwinch.jpg" alt="Bulldog Winch" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.bulldogwinch.com/" target="_new">Bulldog Winch</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://camlockeronline.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/Cam-locker-logo.jpg" alt="Cam-locker Tool Boxes" width="100" height="100"
+            border="2">
+        </a>
+        <figcaption>
+          <a href=" " target="_new">Cam-locker Tool Boxes</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.carr.com" target="_new">
+          <img src="/files/2011/11/carr.jpg" alt="Carr Steps and Accessories" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.carr.com" target="_new">Carr Steps and Accessories</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.covercraft.com" target="_new">
+          <img src="/files/2011/11/cover_craft.jpg" alt="CoverCraft" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.covercraft.com" target="_new">CoverCraft</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.curtmfg.com/" target="_new">
+          <img src="/files/2011/11/curtmfg.jpg" alt="Curt Manufacturing" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.curtmfg.com/" target="_new">Curt Manufacturing</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.deezee.com" target="_new">
+          <img src="/files/2011/11/deezee.jpg" alt="DeeZee Truck Accessories" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.deezee.com" target="_new">DeeZee Truck Accessories</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.draw-tite.com" target="_new">
+          <img src="/files/2011/11/drawtite.jpg" alt="DrawTite" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.draw-tite.com" target="_new">DrawTite</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.eccolink.com/ProductPages/Products.cfm" target="_new">
+          <img src="/files/2011/11/ecco.jpg" alt="ECCO" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.eccolink.com/ProductPages/Products.cfm" target="_new">ECCO</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.edgeproducts.com" target="_new">
+          <img src="/files/2011/11/edge.jpg" alt="Edge Products" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.edgeproducts.com" target="_new">Edge Products</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://extang.com/" target="_new">
+          <img src="/files/2011/11/extang.jpg" alt="Extang" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://extang.com/" target="_new">Extang</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.fabfours.com/Home" target="_new">
+          <img src="/files/2011/11/fabfours.jpg" alt="Fab Fours" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.fabfours.com/Home" target="_new">Fab Fours</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.tuthill.com/us/en/products/transferpumps.cfm" target="_new">
+          <img src="/files/2011/11/fill-rite.jpg" alt="Fill Rite" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.tuthill.com/us/en/products/transferpumps.cfm" target="_new">Fill Rite</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.goindustries.com" target="_new">
+          <img src="/files/2011/11/goind.jpg" alt="Go Industries" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.goindustries.com" target="_new">Go Industries</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.golight.com" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2013/03/Golight-150x150.jpg" alt="Golight" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.golight.com" target="_new">Golight</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.gorhino.com/" target="_new">
+          <img src="/files/2011/11/gorhino.jpg" alt="Gorhino" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.gorhino.com/l" target="_new">Gorhino</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://gpi.net/" target="_new">
+          <img src="/files/2011/12/gpi.jpg" alt="Great Plains" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://gpi.net/" target="_new">Great Plains</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.handyindustries.com" target="_new">
+          <img src="/files/2011/11/handy.jpg" alt="Handy Industries" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.handyindustries.com" target="_new">Handy Industries</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.hopkinsmfg.com" target="_new">
+          <img src="/files/2011/11/hopkin.jpg" alt="Hopkins Manufacturing" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.hopkinsmfg.com" target="_new">Hopkins Manufacturing</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.huskyliners.com" target="_new">
+          <img src="/files/2011/11/husky.jpg" alt="Husky Liners" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.huskyliners.com" target="_new">Husky Liners</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.knaacksecure.com" target="_new">
+          <img src="/files/2011/11/knaack.jpg" alt="KNAACK" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.knaacksecure.com" target="_new">KNAACK</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://mobarmor.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/mob-armor-logo.png" alt="Mob Armor" width="100" height="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://mobarmor.com" target="_new">Mob Armor</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.perry-co.com" target="_new">
+          <img src="/files/2011/11/perry.jpg" alt="Perry Company" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.perry-co.com" target="_new">Perry Company</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.pmlights.com" target="_new">
+          <img src="/files/2011/11/peterson.jpg" alt="Peterson Manufacturing" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.pmlights.com" target="_new">Peterson Manufacturing</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.pilotautomotive.com/" target="_new">
+          <img src="/files/2011/11/pilot.jpg" alt="Pilot Automotive" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.pilotautomotive.com/" target="_new">Pilot Automotive</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.popuphitch.com/" target="_new">
+          <img src="/files/2011/11/pop-up.jpg" alt="Pop Up Towing Products" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.popuphitch.com/" target="_new">Pop Up Towing Products</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.norstarcompany.com/truck_beds.cfm" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/norstarlogo.png" alt="Norstar Truck Beds" width="100" height="75"
+            border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.norstarcompany.com/truck_beds.cfm" target="_new">Norstar Truck Covers</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.piusi.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/Piusi_logo.png" alt="Piusi Tools" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.piusi.com/" target="_new">Piusi pumps</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.putco.com/" target="_new">
+          <img src="/files/2011/11/putco.jpg" alt="Putco" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.putco.com/" target="_new">Putco</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.ranchhand.com/" target="_new">
+          <img src="/files/2011/11/ranch-hand1.jpg" alt="Ranch Hand" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.ranchhand.com/" target="_new">Ranch Hand</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.raptorseries.com" target="_new">
+          <img src="/files/2011/11/raptorseries.gif" alt="Raptor Series" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.raptorseries.com" target="_new">Raptor Series</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.readylift.com/" target="_new">
+          <img src="/files/2011/11/ready-lift.jpg" alt="Ready Lift" width="100" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.readylift.com/" target="_new">Ready Lift</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.retrax.com ">
+          <img style="border: 2px solid black;" src="/files/2011/11/rolltopcover.gif" alt="RETRAX" width="100" height="20" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.retrax.com" target="_new">RETRAX</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.ride-rite.com" target="_new">
+          <img src="/files/2011/11/riderite.jpg" alt="Ride-Rite" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.ride-rite.com" target="_new">Ride-Rite</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.rigidindustries.com ">
+          <img src="http://ette.tv/wp-content/uploads/2013/03/RIGID-INDUSTRIES-LOGO-2.jpeg-150x150.jpg" alt="Rigid Industries"
+            width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.rigidindustries.com ">Rigid Industries</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.rki-us.com" target="_new">
+          <img src="/files/2011/11/rki.jpg" alt="RKI Toolboxes" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.rki-us.com" target="_new">RKI Toolboxes</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.ruggedliner.com/" target="_new">
+          <img src="/files/2011/11/ruggedliner.jpg" alt="Rugged Liner" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.ruggedliner.com/" target="_new">Rugged Liner</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.stampedeproducts.com" target="_new">
+          <img src="/files/2011/11/stampede.jpg" alt="Stampede Products" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.stampedeproducts.com" target="_new">Stampede Products</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.superchips.com" target="_new">
+          <img src="/files/2011/11/superchips.jpg" alt="SuperChips" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.superchips.com" target="_new">SuperChips</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.supersprings.com" target="_new">
+          <img src="/files/2011/11/supersprings.jpg" alt="SuperSprings" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.supersprings.com" target="_new">SuperSprings</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.truckvault.com/dealers/EastTexasTruck" target="_new">
+          <img src="/files/2011/11/truckvault.jpg" alt="Truck Vault" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.truckvault.com/dealers/EastTexasTruck" target="_new">Truck Vault</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="https://www.undercoverinfo.com/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/undercover.png" alt="Undercover Bed Covers" width="100" height="75"
+            border="2">
+        </a>
+        <figcaption>
+          <a href="https://www.undercoverinfo.com/" target="_new">Undercover Bed Covers</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.uwsta.com/k/homepage/" target="_new">
+          <img src="http://ette.tv/wp-content/uploads/2011/11/uws_logo.jpg" alt="UWS Tool Boxes" width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.uwsta.com/k/homepage/" target="_new">UWS Tool Boxes</a>
+        </figcaption>
+      </figure>
+      <figure class="product-lines-item">
+        <a href="http://www.warn.com" target="_new">
+          <img src="/files/2011/11/warn.jpg" alt="Warn Industries, Inc." width="100" height="75" border="2">
+        </a>
+        <figcaption>
+          <a href="http://www.warn.com" target="_new">Warn Industries, Inc.</a>
+        </figcaption>
+      </figure>
+    </article>
+
+    <!-- <img class="home-elite" src="assets/images/home/eliteautoassociation.png" alt="Elite Auto Association"> -->
+  </main>
+  <aside class="main-content-after"></aside>
+</div>
+
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
