72dee0e87912ab1bdbb5afde29a47c7d39c35901
webstandardcss <webstandardcss@gmail.com>
2018-05-19 22:46:56 -0500

    assets files from project

---
+++ assets/js/et-main.js
@@ -0,0 +1,33 @@
+// East Texas Truck Equipment Javascript
+$(document).ready(function () {
+});
+
+$(document).ready(function () {
+  // // Mobile navigation
+  // var dropdownSelect = $(".navselect");
+  // var dropdownButton = $(".mobilenav-button");
+  // var dropdownLength = $(".navselect > option").length;
+  // var pageBody = $(".ob-main");
+  // // Hide the menu on page load
+  // dropdownSelect.hide();
+  // // When clicking menu button, hide the button and show the menu.
+  // dropdownButton.click(function (e) {
+  //  dropdownSelect.toggle().focus().attr('size', dropdownLength);
+  // });
+  // // When clicking menu, hide the clicked menu and show the button.
+  // dropdownSelect.click(function () {
+  //   dropdownSelect.hide();
+  //   dropdownButton.show();
+  // });
+  // // Follow link from the selected option value
+  // dropdownSelect.change(function () {
+  //   window.location = $(this).find("option:selected").val();
+  // });
+});
+
+// TODO Create PHP Pages - DONE
+// TODO Create Sample Content Page Areas for Luis to use when making pages DONE
+// TODO Create Contact Form Areas for Luis to use when making forms DONE
+// TODO Implement Tipue Search or Google API search DONE
+// http://www.tipue.com/beaty/ DONE
+// http://www.tipue.com/search/ DONE
---
+++ assets/js/tipuesearch_content.js
@@ -0,0 +1,13 @@
+
+/*
+Tipue Search 6.1
+Copyright (c) 2017 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+
+// Generated using http://www.tipue.com/beaty/
+var tipuesearch = {"pages": [
+	{"title":"East Texas Truck Equipment", "text": "East Texas Truck Equipment", "tags": "", "url": "http://ette.tv/"},
+]};
+
