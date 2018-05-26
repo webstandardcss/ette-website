3d4c38ad2422c328128f789088b584372a198156
webstandardcss <webstandardcss@gmail.com>
2018-05-25 23:28:11 -0500

    give gallery same template as blank.php

--- gallery.php
+++ gallery.php
@@ -2,19 +2,28 @@
 
 <?php // Page data
 $pageTitle = "Gallery";
-$pageTitleTag = $siteName . " " . $pageTitle;
+$pageTitleTag = $pageTitle . " " . $siteName;
 $pageDescription = $siteName  . "  " . $pageTitle;
 $pageKeywords = $pageTitle . "  " . $siteKeywords;
 // $extraCss = array("forms");
 ?>
 
 <?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
-  <main class="ob-main flex-container">
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+
+<div class="main-outer">
+  <aside class="main-content-before"></aside>
+  <main class="main-content">
     <h1>ETTE Gallery</h1>
+    <h4 class="main-description">At East Texas Truck Equipment </h4> 
+  
+    
     
 	  <div class="product-small">
         <h2><a href="/obf/photo-gallery" target="_blank">Gallery</a></h2>
     </div>
     
   </main>
+  <aside class="main-content-after"></aside>
+</div>
 <?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
