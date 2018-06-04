697cee488a17c919d111eafee0b2c972985a840a
webstandardcss <webstandardcss@gmail.com>
2018-06-03 22:59:35 -0500

    try to fix contact form

--- contact-us.php
+++ contact-us.php
@@ -30,6 +30,7 @@ $extraCss = array("forms");
     </article>
     <div class="form gform-contact">
       <div class="gform_wrapper" id="gform_wrapper_1">
+
       <form method="post" enctype="multipart/form-data" id="gform_1" action="http://new.ette.tv/obf/mail/mail.php">
         <input type="hidden" name="recipients" value="myaddress" />
             <input type="hidden" name="good_url" value="/" />
@@ -97,7 +98,7 @@ $extraCss = array("forms");
               <li id="field_1_6" class="gfield field_sublabel_below field_description_below gfield_visibility_visible">
                 <label class="gfield_label" for="input_1_6">Captcha</label>
                 <div class="ginput_container imgverify">
-                  <img src="http://outbackfeeders.com/obf/mail/verifyimg.php" alt="Image verification" name="vimg" />
+                  <img src="http://new.ette.tv/obf/mail/verifyimg.php" alt="Image verification" name="vimg" />
                   <input type="text" size="12" name="imgverify" placeholder="Enter image text" />
                 </div>
               </li>
